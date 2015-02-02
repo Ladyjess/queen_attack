e0851d9eb4427bae862855edd80f8ceed1ec5bc4
jessica hori <student@epicodus.com>
2015-02-02 14:15:09 -0800

    Edit readme

---
+++ README.md
@@ -0,0 +1,3 @@
+#Queen Attack Confirmation
+
+This will tell you whether or not the position at [num , num] can attack another position at [i, i]
--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,7 +1,20 @@
 class Array
+define_method(:queen_attack?) do |opponent|
 
+  xdifference = opponent.at(0).-(self.at(0))
+  ydifference= opponent.at(1).-(self.at(1))
 
-define_method(:queen_attack?) do |position_array|
+  xposition = xdifference.abs
+  yposition = ydifference.abs
 
-
+  if self.at(0).eql?(opponent.at(0))
+    true
+  elsif self.at(1).eql?(opponent.at(1))
+    true
+  elsif xposition.eql?(yposition)
+    true
+  else
+    false
+  end
+end
 end
